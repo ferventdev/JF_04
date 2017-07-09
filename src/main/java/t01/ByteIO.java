adba3c658ddd0f8a0a69f290e0f0a0a313b3a222
ferventdev <alex.4everyours@gmail.com>
2017-07-09 19:10:15 +0300

    Task 1 - countKeywords() done

--- src/main/java/t01/ByteIO.java
+++ src/main/java/t01/ByteIO.java
@@ -1,15 +1,29 @@
 package t01;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Aleksandr Shevkunenko on 07.07.2017.
  * 777_abc - check for the wrong Java identifier
  */
 public class ByteIO {
+
+    static final String[] javaKeywords = {
+            "abstract", "continue", "for", "new", "switch",
+            "assert", "default", "goto", "package", "synchronized",
+            "boolean", "do", "if", "private", "this",
+            "break", "double", "implements", "protected", "throw",
+            "byte", "else", "import", "public", "throws",
+            "case", "enum", "instanceof", "return", "transient",
+            "catch", "extends", "int", "short", "try",
+            "char", "final", "interface", "static", "void",
+            "class", "finally", "long", "strictfp", "volatile",
+            "const", "float", "native", "super", "while"
+    };
 
     static List<String> getWords(String filename) {
         List<String> words = new ArrayList<>();
@@ -46,4 +60,17 @@ public class ByteIO {
         }
         return words;
     }
+
+    static Map<String, Integer> countKeywords(List<String> allWords) {
+        Set<String> keywords = Stream.of(javaKeywords).collect(Collectors.toSet());
+        Map<String, Integer> dict = new HashMap<>();
+        for (String word : allWords) {
+            if (keywords.contains(word)) {
+                Integer newValue = dict.getOrDefault(word, 0) + 1;
+                dict.put(word, newValue);
+            }
+        }
+        return dict;
+    }
+
 }
--- src/test/java/t01/ByteIOTest.java
+++ src/test/java/t01/ByteIOTest.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,11 +23,12 @@ public class ByteIOTest {
     @Test
     public void getWordsTest() throws Exception {
         String[] wordsArray = new String(Files.readAllBytes(Paths.get(filename))).split("[^\\w$]");
-        List<String> words = Arrays.stream(wordsArray)
+        List<String> words1 = Arrays.stream(wordsArray)
                 .filter(str -> !str.isEmpty() && Character.isJavaIdentifierStart(str.charAt(0)))
                 .collect(Collectors.toList());
+        List<String> words2 = ByteIO.getWords(filename);
 
-        assertThat(ByteIO.getWords(filename), is(words));
+        assertThat(words2, is(words1));
     }
 
 }
