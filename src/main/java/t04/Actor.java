cde52bda04cf105b5086e208d66393b268d97fd2
ferventdev <alex.4everyours@gmail.com>
2017-07-11 15:46:19 +0300

    Task 4 - Actor class created

---
+++ src/main/java/t04/Actor.java
@@ -0,0 +1,13 @@
+package t04;
+
+import lombok.Value;
+
+/**
+ * Created by Aleksandr Shevkunenko on 11.07.2017.
+ */
+@Value
+public class Actor {
+    private String firstName;
+    private String lastName;
+    private String nationality;
+}
